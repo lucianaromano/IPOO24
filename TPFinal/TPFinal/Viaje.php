d277fe70ee3d8d993e48b782685c09e3278f7188
lucianaromano <luciana.romano@fi.uncoma.edu.ar>
2024-06-18 22:59:06 -0300

    cambios

---
+++ TPFinal/TPFinal/Viaje.php
@@ -0,0 +1,247 @@
+<?php
+include_once 'BaseDatos.php';
+/**El viaje ahora contiene una referencia a una colección de objetos de la clase Pasajero.
+ *  La clase Viaje debe hacer referencia al responsable de realizar el viaje.*/
+
+class Viaje {
+    private $idViaje;
+    private $vdestino;
+    private $vcantMaxPasajeros;
+    private $vcolPasajeros;
+    private $rnumeroEmpleado; //obj responsable
+    private $vimporte;
+    private $idEmpresa; //objeto empresa
+    private $mensajeOperacion;
+
+
+    
+    //metodo constructor de la clase
+    public function __construct(){
+        $this-> idViaje = '';
+        $this-> vdestino='';
+        $this-> vcantMaxPasajeros= '';
+        $this-> vcolPasajeros='';
+        $this-> rnumeroEmpleado= '';
+        $this-> vimporte = '';
+        $this-> mensajeOperacion = '';
+    }
+
+    public function getIdViaje (){
+        return $this->idViaje;
+    }
+
+    public function setIdViaje($cod){
+        $this->idViaje=$cod;
+    }
+
+    public function getVdestino (){
+        return $this->vdestino;
+    }
+
+    public function setVdestino($dest){
+        $this->vdestino=$dest;
+    }
+
+    public function getVcantMaxPasajeros(){
+        return $this-> vcantMaxPasajeros;
+    }
+
+    public function setVcantMaxPasajeros($cantMax){
+        $this->vcantMaxPasajeros=$cantMax;
+    }
+
+    public function getVcolPasajeros(){
+        return $this-> vcolPasajeros;
+    }
+
+    public function setVcolPasajeros ($vcolPasajeros){
+        $this->vcolPasajeros=$vcolPasajeros;
+    }
+
+    public function getRnumeroempleado(){
+        return $this-> rnumeroEmpleado;
+    }
+
+    public function setRnumeroempleado($rnumeroEmpleado){
+        $this->rnumeroEmpleado=$rnumeroEmpleado;
+    }
+
+    public function getMensajeOperacion(){
+        return $this->mensajeOperacion;
+    }
+
+    public function setMensajeOperacion($nuevo){
+        $this->mensajeOperacion = $nuevo;
+    }
+
+    public function getVimporte(){
+        return $this->vimporte;
+    }
+    public function setVimporte($vimporte){
+        $this->vimporte = $vimporte;
+    }
+
+    public function getIdEmpresa(){
+        return $this->idEmpresa;
+    }
+    public function setIdEmpresa($idEmpresa){
+        $this->idEmpresa = $idEmpresa;
+    }
+
+    /**
+     * Retorna la coleccion de funciones en forma de cadena de caracteres
+     */
+    private function funcionesAString()
+    {
+        //Array Funcion $arreglo
+        //String $retorno
+        $retorno = "";
+        $arreglo = $this->getColObjPasajeros();
+        foreach ($arreglo as $i) {
+            $retorno .= $i . "\n";
+            $retorno .= "----------------------------------------------------------------------\n";
+        }
+        return $retorno;
+    }
+    //toString
+    public function __toString()
+    {
+        return "----------------------------------
+            ID: " . $this->getIdViaje() .
+            "\nvDestino: " . $this->getVdestino() .
+            "\nCantidad maxima de pasajeros: " . $this->getVcantMaxPasajeros() .
+            "\nPasajeros: \n" . $this->funcionesAString() .
+            "\nEmpleado Responsable: \n" . $this->getRnumeroEmpleado() .
+            "\nImporte: $" . $this->getVimporte() .
+            "\nEmpresa: " . $this->getIdEmpresa()."\n";
+    }
+
+    //FUNCIONES BD
+    public function cargar($idViaje,$vdestino,$vcantMaxPasajeros,$rnumeroEmpleado,$vimporte,$idEmpresa){
+        $this->setIdViaje($idViaje);
+        $this->setVdestino($vdestino);
+        $this->setVcantMaxPasajeros($vcantMaxPasajeros);
+        $this->setRnumeroempleado($rnumeroEmpleado);
+        $this->setVimporte($vimporte);
+        $this->setIdEmpresa($idEmpresa);
+    }
+
+    public function Buscar($id){
+        $base = new BaseDatos();
+        $rta = false;
+        $consulta = "SELECT * FROM Viaje WHERE idViaje=" . $id;
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($consulta)) {
+                if ($row2 = $base->Registro()) {
+                    $this->setIdViaje($row2['idViaje']);
+                    $this->setvDestino($row2['vvdestino']);
+                    $this->setCantMaxPasajeros($row2['vcantmaxpasajeros']);
+                    $pasajero = new Pasajero();
+                    $pasajero->buscar($row2['pdocumento']);
+                    $this->setColObjPasajeros($pasajero);
+                    $responsable = new Responsable();
+                    $responsable->buscar($row2['rnumeroempleado']);
+                    $this->setRnumeroempleado($responsable);
+                    $this->setVimporte($row2['vimporte']);
+                    $this->setIdEmpresa($row2['idempresa']);
+                    $rta = true;
+                }
+            } else {
+                $this->setMensajeOperacion($base->getError());
+            }
+        } else {
+            $this->setMensajeOperacion($base->getError());
+        }
+        return $rta;
+    }
+
+    /**Lista todas los viajes que cumplen con cierta condicion 
+	 * @param void 
+     * @return array 
+	 */
+	public function listar($condicion = ""){
+	    $arrayViaje = null;
+		$base = new BaseDatos();
+		$consultaViajes = "SELECT * from viaje ";
+		if ($condicion != ""){
+		    $consultaViajes = $consultaViajes.' where '.$condicion;
+		}
+		$consultaViajes.=" order by idviaje ";
+
+		if($base->Iniciar()){
+			if($base->Ejecutar($consultaViajes)){				
+				$arrayViaje = array();
+				while($row2 = $base->Registro()){
+					$viaje = new viaje();
+					$viaje->Buscar($row2['idviaje']);
+					array_push($arrayViaje,$viaje);
+				}				
+            }else{
+		 		$this->setMensajeOperacion($base->getError()); 		
+			}
+		 }else{
+		 	$this->setMensajeOperacion($base->getError());
+		}	 
+		return $arrayViaje;
+	}
+
+    public function insertar(){
+        $base = new BaseDatos();
+        $rta = false;
+        $empresa = $this->getIdempresa();
+        $idEmpresa = $empresa->getIdEmpresa();
+        $responsable = $this->getRnumeroempleado();
+        $numResponsable = $responsable->getrnumeroEmpleado();
+        $consulta = "INSERT INTO viaje(vdestino, vcantmaxpasajeros,colObjPasajeros, idempresa, rnumeroempleado, vimporte, " .
+        "tipoAsiento, idayvuelta) VALUES ('{$this->getVdestino()}', {$this->getVcantmaxpasajeros()}, 
+        {$idEmpresa}, {$numResponsable}, {$this->getVimporte()})";
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($consulta)) {
+                $rta = true;
+            } else {
+                $this->setMensajeOperacion($base->getError());
+            }
+        } else {
+            $this->setMensajeoperacion($base->getError());
+        }
+        return $rta;
+    }
+
+    public function modificar()
+    {
+        $rta = false;
+        $base = new BaseDatos();
+        $empresa = $this->get();
+        $idEmpresa = $empresa->getIdempresa();
+        $responsable = $this->getRnumeroempleado();
+        $numResponsable = $responsable->getRnumeroempleado();
+        $consulta = "UPDATE viaje SET vdestino = '{$this->getVdestino()}', vcantmaxpasajeros = {$this->getVcantmaxpasajeros()}, idempresa = {$idEmpresa}, rnumeroempleado = {$numResponsable}, vimporte = {$this->getVimporte()}, tipoAsiento = '{$this->getTipoasiento()}', idayvuelta = '{$this->getIdayvuelta()}' WHERE idviaje = {$this->getIdviaje()}";
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($consulta)) {
+                $rta = true;
+            } else {
+                $this->setMensajeoperacion($base->getError());
+            }
+        } else {
+            $this->setMensajeoperacion($base->getError());
+        }
+        return $rta;
+    }
+
+    public function eliminar()
+    {
+        $base = new BaseDatos();
+        $rta = false;
+        $consulta = "DELETE FROM viaje WHERE idviaje = " . $this->getIdviaje();
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($consulta)) {
+                $rta = true;
+            } else {
+                $this->setMensajeoperacion($base->getError());
+            }
+        } else {
+            $this->setMensajeoperacion($base->getError());
+        }
+        return $rta;
+    }
+}
